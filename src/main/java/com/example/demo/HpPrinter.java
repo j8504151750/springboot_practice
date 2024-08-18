002761729d3c71f6911ec66818c66ee84dd44a4b
j8504151750@gmail.com <j8504151750@gmail.com>
2024-08-18 19:54:38 +0800

    @value應用

--- src/main/java/com/example/demo/HpPrinter.java
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -1,22 +1,24 @@
 package com.example.demo;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HpPrinter implements Printer{
 
+    @Value("${printer.name}")
+    private String name;
+
+    @Value("${printer.count:20}")
     private int count ;
 
-    @PostConstruct
-    public void init() {
-        count = 5;
-    }
+
 
     @Override
     public void print(String messge) {
         count -- ;
-        System.out.println("HP Printer :" + messge);
+        System.out.println(name + ": " + messge);
         System.out.println("剩餘使用次數" + count);
     }
 }
