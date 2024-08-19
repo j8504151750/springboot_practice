16df5e926fef05f690657007ed6fe79d54120f5e
j8504151750 <j8504151750@gmail.com>
2024-08-19 16:23:30 +0800

    結構化的呈現數據JSON

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import java.util.List;
+
+public class Student {
+    Integer id;
+    String name;
+    Double score;
+    Boolean graduated;
+    List<String> courseList;
+    Pet pet;
+    List<Pet> petList;
+}
